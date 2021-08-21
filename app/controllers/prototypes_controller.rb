e3b7c000c91243596fb1023c9de6eafcd0777fcc
溝上達朗 <mizogamitatsurou@mizokamitatsurounoiMac.local>
2021-08-22 01:20:24 +0900

    second commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :move_to_index, except: [:index, :show, ]
+  before_action :move_to_index, only: [:edit ]
 
   def index
     @prototypes = Prototype.all
@@ -54,9 +54,10 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless @prototype == current_user
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to action: :index
     end
-  end
+  end 
   
 end
